337632aefbc588c9e40919afcca1fec7f95af6f8
Li Kang <wlikangw@gmail.com>
2019-04-02 14:09:57 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,55 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const WebpackManifestPlugin = require('webpack-manifest-plugin')
 
 module.exports = {
   entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname,'dist')
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    }),
+    new CleanWebpackPlugin(),
+    new WebpackManifestPlugin()
+  ]
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.css$/,
+  //       use: [
+  //         {
+  //           loader: 'style-loader',
+  //           options: {
+  //             hmr: false
+  //           }
+  //         },
+  //         {
+  //           loader: 'css-loader',
+  //         }
+  //       ]
+  //     },
+  //     {
+  //       test: /\.(png|svg|jpg|gif)$/,
+  //       use: [
+  //         {
+  //           loader: 'file-loader'
+  //         }
+  //       ]
+  //     },
+  //     {
+  //            test: /\.(woff|woff2|eot|ttf|otf)$/,
+  //             use: [
+  //                'file-loader'
+  //                ]
+  //              }
+  //   ]
+  // }
 }
